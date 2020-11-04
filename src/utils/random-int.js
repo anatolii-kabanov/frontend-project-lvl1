34e2d468ff9d3f09b493716061d8f2946b28543c
Anatolii Kabanov <4akabanov@gmail.com>
2020-11-04 19:44:35 +0100

    Reworked getRandomInt function.

--- src/utils/random-int.js
+++ src/utils/random-int.js
@@ -1,4 +1,12 @@
-const getRandomInt = (max = Number.MAX_SAFE_INTEGER, startNumber = 0) => (
-  Math.floor(Math.random() * Math.floor(max)) + startNumber
-);
+const getRandomInt = (max = Number.MAX_SAFE_INTEGER, startNumber = 0) => {
+  const minNumber = Math.floor(startNumber);
+  const maxNumber = Math.floor(max);
+  if (maxNumber < minNumber) {
+    throw new Error(
+      `Min number: ${minNumber}, can't be greate than max number: ${maxNumber}`,
+    );
+  }
+  const difference = maxNumber - minNumber;
+  return Math.floor(Math.random() * (difference + 1)) + minNumber;
+};
 export default getRandomInt;
