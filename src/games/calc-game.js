52bb352630d4b67ce4fa496a065291b9201f840f
Anatolii Kabanov <4akabanov@gmail.com>
2020-11-07 13:51:41 +0100

    Brain-calc: using array of math operations, return number in calculate function, convert to string in one place.

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,34 +1,30 @@
 import runGameEngine from '../common/game-engine.js';
 import getRandomInt from '../utils/random-int.js';
 
-const mathSigns = {
-  0: '+',
-  1: '-',
-  2: '*',
-};
+const mathSigns = ['+', '-', '*'];
 
 const maxGeneratedNumber = 100;
 
 const calculate = (firstNumber, secondNumber, sign) => {
   switch (sign) {
     case '+':
-      return `${firstNumber + secondNumber}`;
+      return firstNumber + secondNumber;
     case '-':
-      return `${firstNumber - secondNumber}`;
+      return firstNumber - secondNumber;
     case '*':
-      return `${firstNumber * secondNumber}`;
+      return firstNumber * secondNumber;
     default:
-      return null;
+      throw new Error(`Math operation doesn't exist: ${sign}`);
   }
 };
 
 const generateQuestionAndAnswer = () => {
   const firstNumber = getRandomInt(maxGeneratedNumber);
   const secondNumber = getRandomInt(maxGeneratedNumber);
-  const signToUse = mathSigns[getRandomInt(2)];
+  const signToUse = mathSigns[getRandomInt(mathSigns.length - 1)];
   return {
     question: `${firstNumber} ${signToUse} ${secondNumber}`,
-    answer: calculate(firstNumber, secondNumber, signToUse),
+    answer: calculate(firstNumber, secondNumber, signToUse).toString(),
   };
 };
 
